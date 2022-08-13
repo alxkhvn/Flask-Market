087b7975af67e839559ba92897b62c08627bec28
alikhan <alikhan@gmail.com>
2022-08-13 18:40:02 +0600

    Added registration html file and route. Added forms with validation

--- market/__init__.py
+++ market/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '6d240e5c6d7abfad820da27f'
 db = SQLAlchemy(app)
 
 from market import routes
